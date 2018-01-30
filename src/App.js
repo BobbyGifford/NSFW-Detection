32f62dcb30a4d717d448863af713a54af4e467c6
Bobby <bobby077joe@gmail.com>
2018-01-29 18:46:27 -0800

    Data properly formatted. App needs style refinement but otherwise working great.

--- src/App.js
+++ src/App.js
@@ -1,64 +1,79 @@
-import React, { Component } from 'react';
-import Clarifai from 'clarifai';
-import './App.css';
+import React, { Component } from "react";
+import Clarifai from "clarifai";
+import "./App.css";
 
-import InputHeader from './components/InputHeader/InputHeader';
-import InputField from './components/InputField/InputField';
-import OutputImage from './components/OutputImage/OutputImage';
-import OutputRating from './components/OutputRating/OutputRating';
+import InputHeader from "./components/InputHeader/InputHeader";
+import InputField from "./components/InputField/InputField";
+import OutputImage from "./components/OutputImage/OutputImage";
+import OutputRating from "./components/OutputRating/OutputRating";
 import apiKey from "./config/dev";
 
 // nsfw value response.outputs[0].data.concepts[0].value
 // sfw value response.outputs[0].data.concepts[1].value
 
 const app = new Clarifai.App({
-  apiKey: apiKey.clarifaiKey,
+  apiKey: apiKey.clarifaiKey
 });
 
-
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
       input: "",
       imageUrl: "",
-      nsfwRating: null,
-      sfwRating: null
-    }
+      firstIndexData: "",
+      secondIndexData: ""
+    };
   }
 
-  onChange = (event) => {
-    this.setState({input: event.target.value})
-  }
+  onChange = event => {
+    this.setState({ input: event.target.value });
+  };
 
-  setRatingData = (data) => {
-    const nsfwRating = data.outputs[0].data.concepts[0].value;
-    const sfwRating = data.outputs[0].data.concepts[1].value;
-    
-    this.setState({ sfwRating })
-    this.setState({ nsfwRating })
-  }
+  setRatingData = data => {
+    const firstIndexData = data.outputs[0].data.concepts[0];
+    const secondIndexData = data.outputs[0].data.concepts[1];
 
-  onSubmit = (event) => {
+    this.setState({ firstIndexData });
+    this.setState({ secondIndexData });
+  };
+
+  onSubmit = event => {
     event.preventDefault();
     // e.target.reset();
-    this.setState({imageUrl: this.state.input})
-    app.models.predict(Clarifai.NSFW_MODEL, this.state.input).then((response) => {
+    this.setState({ imageUrl: this.state.input });
+    app.models.predict(Clarifai.NSFW_MODEL, this.state.input).then(
+      response => {
         this.setRatingData(response);
+        console.log(response);
+
+        console.log(response.outputs[0].data.concepts[0]);
+        console.log(response.outputs[0].data.concepts[1]);
       },
       function(err) {
         console.log(err);
       }
-    );  }
+    );
+  };
 
   render() {
     return (
       <div className="App">
-      <InputHeader />
-      <InputField onChange={this.onChange} onSubmit={this.onSubmit}/>
-      {this.state.imageUrl ? <OutputImage imageUrl={this.state.imageUrl} /> : null}
-      {this.state.nsfwRating ? <OutputRating nsfwRating={this.state.nsfwRating} sfwRating={this.state.sfwRating}/> : null}
+        <InputHeader />
+        <InputField onChange={this.onChange} onSubmit={this.onSubmit} />
+        {this.state.imageUrl ? (
+          <OutputRating
+            firstIndexName={this.state.firstIndexData.name}
+            firstIndexValue={this.state.firstIndexData.value}
+            secondIndexName={this.state.secondIndexData.name}
+            secondIndexValue={this.state.secondIndexData.value}
+          />
+        ) : null}
+
+        {this.state.imageUrl ? (
+          <OutputImage imageUrl={this.state.imageUrl} />
+        ) : null}
       </div>
     );
   }
--- src/components/OutputRating/OutputRating.js
+++ src/components/OutputRating/OutputRating.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const OutputRating = ({ nsfwRating, sfwRating}) => {
+const OutputRating = ({ firstIndexName, firstIndexValue, secondIndexName, secondIndexValue}) => {
     return (
         <div className='mt-3'>
-            <p>NSFW Rating: {Math.round(nsfwRating * 100)}%</p>
-            <p>SFW Rating: {Math.round(sfwRating * 100)}%</p>
+            <p>{firstIndexName} rating: {Math.round(firstIndexValue * 100)}%</p>
+            <p>{secondIndexName} rating: {Math.round(secondIndexValue * 100)}%</p>
         </div>
     )
 }
